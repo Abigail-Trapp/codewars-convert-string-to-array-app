693be6a2ee4ab5014b1fc9d9f00f4b3a8aa9d1e9
Abigail Trapp <abbsthepandahead@gmail.com>
2021-11-08 21:49:24 -0700

    final commit

--- code.js
+++ code.js
@@ -3,4 +3,15 @@ function stringToArray(string){
       // code code code
     
     }
+  let button = document.getElementById("button")
+  button.addEventListener("click", submit)
+
+  function submit(event){
+    event.preventDefault();
     
+let form = document.querySelector("form")
+   let result = document.getElementById("result_input").value;
+  let output = document.createElement("div")
+  output.innerHTML = `[${stringToArray(result)}]`
+  form.append(output)
+  }
